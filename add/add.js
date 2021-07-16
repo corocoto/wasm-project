321bc01970b27e92bf85d9972a9a8aa851393bbd
Artem Gusev <gusev2014russia@mail.ru>
2021-07-16 08:49:08 +0300

    adds example of using for node environment

--- add/add.js
+++ add/add.js
@@ -1,3 +1,17 @@
 const fs = require('fs');
-const bytecode = fs.readFileSync('add.wasm');
-let wasm = (async()=>await WebAssembly.instantiate(bytecode))();
+
+async function run () {
+    try {
+        const bytecode = fs.readFileSync(`${__dirname}/add.wasm`);
+        const wasm = await WebAssembly.instantiate(bytecode);
+        console.log(wasm.instance.exports.addInt32(1,2));
+        // console.log(wasm.instance.exports.addInt64(1,2));
+        console.log(wasm.instance.exports.addFloat32(1.1,2.1));
+        console.log(wasm.instance.exports.addFloat64(1.1,2.1));
+    }
+    catch(e) {
+        console.error(e);
+    }
+}
+
+run();
